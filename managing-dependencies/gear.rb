5589ed00949d6ecc02413cdb4693f7202c1641f5
Andres Colonia <andreskal@gmail.com>
2018-01-28 20:51:49 -0300

    removing coupling with dependency injection

--- managing-dependencies/gear.rb
+++ managing-dependencies/gear.rb
@@ -1,18 +1,17 @@
 require_relative 'wheel.rb'
 
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = wheel
   end
 
   #TODO Wheel's reference generate a dependency
   def gear_inches
-    ratio * Wheel.new(rim, tire).diameter
+    ratio * wheel.diameter
   end
 
   def ratio
@@ -20,4 +19,4 @@ class Gear
   end
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
