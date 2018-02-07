8f23462db4857891ff9479468769f996bc6ad655
Andres Colonia <andreskal@gmail.com>
2018-02-07 16:53:32 -0300

    remove argument-order dependencies
    use hash to eliminate isolate with order params

--- managing-dependencies/gear.rb
+++ managing-dependencies/gear.rb
@@ -1,20 +1,21 @@
 require_relative 'wheel.rb'
 
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
-    @chainring = chainring
-    @cog = cog
-    @rim = rim
-    @tire = tire
+  def initialize(args)
+    @chainring = args[:chainring]
+    @cog = args[:cog]
+    @wheel = args[:wheel]
   end
 
   def gear_inches
     ratio * wheel.diameter
   end
 
-  def wheel
-    @wheel ||= Wheel.new(rim, tire)
+  def ratio
+    @chainring / @cog.to_f
   end
 end
+
+Gear.new(chainring: 52, cog: 11, wheel: Wheel.new(26, 1.5)).gear_inches
